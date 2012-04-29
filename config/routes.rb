59e059322880c4e2d9e5a114737693ae20de74d1
Dan Ryan <scriptfu@gmail.com>
2012-04-29 00:44:40 -0400

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Mastermind::Application.routes.draw do
+  get "jobs/index"
+
+  get "jobs/show"
+
+  get "jobs/new"
+
+  get "jobs/edit"
+
+  resources :heists do
+    resources :jobs
+  end
+  
+
 end
